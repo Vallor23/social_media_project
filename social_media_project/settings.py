8b274be1c21d01b8e7109c12868a6f4f9905aac9
Vallor23 <enosnyabinda2017@gmail.com>
2025-08-14 06:28:31 +0300

    fix: set debug to False

--- social_media_project/settings.py
+++ social_media_project/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['social-media-project-9u8u.onrender.com', 'localhost', '127.0.0.1']
 
